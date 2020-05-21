c0ce1d05dbd7bb1d507e10cdcbc69119f17b0b04
xjx <xjx@disanbo.com>
2020-05-21 18:42:19 +0800

    更新

--- 00-基础知识/codes/pointer.go
+++ 00-基础知识/codes/pointer.go
@@ -7,7 +7,29 @@
 
 package main
 
+import (
+	"fmt"
+)
+
 func main()  {
-	
+
+	i := 1
+	var p *int // 定义指针方式一
+	p2 := new(int) //定义指针方式二
+	p = &i
+	p2 = &i
+	fmt.Println(p)
+	fmt.Println(p2)
+
+	a := 5
+	pointer1 := &a
+	fmt.Println(*pointer1) // 5 指针取值
+	*pointer1 = 6 // 通过指针修改 a值
+	fmt.Println(a) // 6
+
+	b := 1
+	pointer2 := &b
+	pointer2++
+	fmt.Println(pointer2)
 }
 
