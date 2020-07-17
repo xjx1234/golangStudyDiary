cc1a17aa0231d237d75b0bc30f3aa90a05ccdd19
xjx <xjx@disanbo.com>
2020-07-17 16:16:26 +0800

    更改目录结构

---
+++ 01-Go实例/codes/websocket_client.go
@@ -0,0 +1,8 @@
+/**
+* @Author: XJX
+* @Description:
+* @File: websocket_client.go
+* @Date: 2020/7/14 14:24
+ */
+
+package main
---
+++ 01-Go实例/codes/websocket_server.go
@@ -0,0 +1,12 @@
+/**
+* @Author: XJX
+* @Description:
+* @File: websocket_server.go
+* @Date: 2020/7/14 14:24
+ */
+
+package main
+
+func main() {
+
+}
